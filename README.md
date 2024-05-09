45c6c07732e66f73b3c3dbb608671811d03162b6
sammsaski <samuel.sasaki@gmail.com>
2024-05-09 10:53:02 -0500

    more changes for initial_model

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Neural Automata
 
 We propose a novel model of computation with neural automata. Like classic automata, this model of computation contains a set of states and descriptions of the transitions between these states. Unlike classic automata, every transition of the system is described by a neural network.
+
+# Model
+Neutron
+- Nodes
+  - Name
+  - Neural Network
+- Transitions
+  - Nodes
+  - Neural Network
+  
--- neutron/automata.py
+++ neutron/automata.py
@@ -1,7 +1,8 @@
-from typing import List
+from typing import Any, DefaultDict, List
+import random
 
-class Node:
-    """A node in the neural automata."""
+class Mode:
+    """A mode in the neural automata."""
     def __init__(self, name: str, nn):
         self.name = name
         self.nn = nn
@@ -10,40 +11,77 @@ class Node:
         return self.nn(x)
 
     def __str__(self,):
-        return f'Node `{self.name}`'
+        return f'Mode `{self.name}`'
 
 class Transition:
-    """For defining the logic of a transition between nodes."""
-    pass
+    """For defining the logic of a transition between nodes.
+    We must keep track of the mode we need to transition to
+    """
+    def __init__(self, nn):
+        self.nn = nn
+
+    def __call__(self, input,) -> Any:
+        return self.nn(input)
 
 class Neutron:
     """A neural automata."""
-    def __init__(self, nodes: List, directed=True):
-        """The building of every automata will begin by defining the number of states
+    def __init__(self, modes: List, directed=True):
+        """The building of every automata will begin by defining the number of modes
         desired.
+
+        An automata is made up of:
+        - M : a set of modes
+        - S : a set of state variables
+        - R : a set of transitions between modes
+        - I : a set of initial modes that are a subset of S
+        - X : a set of input variables
+        - O : a set of output variables
         """
-        self.nodes = nodes 
+        self.modes = modes 
         self.edges = {}
 
-        self.num_nodes = len(self.nodes)
+        self.state_vars = DefaultDict()
+
+        self.num_modes = len(self.modes)
         self.num_edges = len(self.edges)
+
+        self.directed = directed
+
+        self.initial_modes= []
+        if len(self.initial_modes) == 1:
+            self.current_mode= self.initial_modes[0]
+        else:
+            self.current_mode= random.choice(self.initial_modes)
     
     def move(self,):
         pass
 
-    def add_node(self, new_node):
-        pass
+    def add_mode(self, new_mode) -> None:
+        self.modes.append(new_mode)
+        self._update_num_modes()
+        return
 
-    def build_edge(self, node1: Node, node2: Node):
-        self.edges[(node1, node2)]
+    def build_edge(self, mode1: Mode, mode2: Mode, t: Transition) -> None:
+        self.edges[(mode2, mode2)] = t
+        self._update_num_edges()
+        return
 
     def check_neutron(self,):
         pass
 
-    def _update_num_nodes(self,):
-        self.num_nodes = len(self.nodes) 
+    def _update_num_modes(self,):
+        self.num_modes= len(self.modes) 
 
     def _update_num_edges(self,):
         self.num_edges = len(self.edges) 
 
+    def __str__(self,):
+        return ''.join([f'{mode1.name} --> {mode2.name}\n' for mode1, mode2 in self.edges.keys()])
 
+if __name__ == "__main__":
+    # build modes 
+    mode1 = Mode(name='Mode 1', nn=None) # leave None for now
+    mode2 = Mode(name='Mode 2', nn=None)
+
+    print(mode1)
+    print(mode2)
